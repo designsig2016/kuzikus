29d23b6f4924b0e4ae7874cb41138c5bdf782e30
Marc Soutter <marc.soutter@epfl.ch>
2016-12-01 13:53:10 +0100

    update app.js and styles

--- kuziweb/app.js
+++ kuziweb/app.js
@@ -25,6 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+// Added ************************************************************************
+
+ app.get('/home',function(req,res){
+  res.sendFile('/views/mapPage.html', {root: __dirname});
+}); 
+
+// ******************************************************************************
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
